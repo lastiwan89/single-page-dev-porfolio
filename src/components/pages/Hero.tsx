71523097ca605cda9b299b9f7d02e2b9454353f4
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 07:50:03 +0900

    mobile ok

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,19 +1,28 @@
 import Button from "../Button";
 import Divider from "../Divider";
-import Header from "./Header";
+import NavLink from "../Navlink";
 
 export default function Hero() {
   return (
-    <main>
-      <div className="hero px-6">
-        <div className="flex flex-col items-center pb-16">
-          <Header />
-          <div className="flex flex-col gap-5">
-            <h1 className="text-pure-white text-fs-40 leading-lh-40 -tracking-ls-1.136 text-center font-bold">
+    <main className="md:h-screen">
+      <div className="hero">
+        <div className="flex flex-col items-center pb-16 md:items-stretch">
+          <div>
+            <NavLink />
+            <div className="relative -mt-40 flex flex-col items-center md:-mt-16 md:items-end">
+              <img
+                className="w-52 md:absolute md:w-80"
+                src="/image-profile-mobile.webp"
+                alt="profile picture"
+              />
+            </div>
+          </div>
+          <div className="mt-8 flex flex-col gap-5">
+            <h1 className="text-pure-white text-fs-40 leading-lh-40 -tracking-ls-1.136 text-center font-bold md:text-start">
               Nice to meet you! I’m{" "}
               <span className="border-green border-b-4">Adam Keyes</span>.
             </h1>
-            <p className="text-pure-white text-fs-16 leading-lh-26 text-center">
+            <p className="text-pure-white text-fs-16 leading-lh-26 text-center md:text-start">
               Based in the UK, I’m a front-end developer passionate about
               building accessible web apps that users love.
             </p>
