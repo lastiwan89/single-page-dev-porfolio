d7cff86df18f6b50b4fd8a3719d0c5433b9ff407
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-31 18:13:47 +0900

    move divider to components

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,11 @@ import Skills from "./components/pages/Skills";
 
 export default function App() {
   return (
-    <main className="max-w-desktop mx-auto bg-black py-6 md:p-8 xl:px-40 xl:py-10">
+    <main className="max-w-desktop mx-auto bg-black py-6 xl:py-10">
       <Hero />
-      <div className="bg-med-white h-[1px] w-full"></div>
       <Skills />
-      <div className="bg-med-white h-[1px] w-full"></div>
       <Project />
-      <div className="bg-med-white h-[1px] w-full"></div>
       <Contact />
-      <div className="bg-med-white h-[1px] w-full"></div>
       <Footer />
     </main>
   );
