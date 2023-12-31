6cf6b1650e6157f188501072f52a05352fcd3286
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-31 23:02:08 +0900

    complete responsive contact section

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -4,7 +4,7 @@ import Divider from "../Divider";
 export default function Contact() {
   return (
     <section className="contact bg-med-black flex flex-col gap-y-12 px-6">
-      <div className="flex flex-col py-16 md:px-40 lg:grid lg:grid-cols-2 xl:px-40">
+      <div className="flex flex-col py-16 md:px-28 lg:grid lg:grid-cols-2 lg:gap-20 lg:px-16 xl:gap-40 xl:px-36">
         <div className="flex flex-col gap-5">
           <h3 className="text-pure-white md:text-fs-72 md:leading-lh-72 md:-tracking-ls-2.045 text-fs-40 leading-lh-40 -tracking-ls-1.136 text-center font-bold lg:text-start">
             Contact
@@ -14,7 +14,7 @@ export default function Contact() {
             fill in the form, and I’ll get back to you as soon as possible.
           </p>
         </div>
-        <form className="grid grid-cols-1 md:mt-6">
+        <form className="flex flex-col">
           <input
             className="placeholder:text-fs-16 placeholder:leading-lh-26 placeholder:-tracking-ls-0.222 border-b-med-white mt-2 border-b-[1px] p-6 placeholder:font-medium placeholder:uppercase"
             type="text"
