afdc1fd27af6cebb0afb3e4e80956eb5bb39cffe
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 14:47:12 +0900

    done bg px md lg

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -3,12 +3,12 @@ import Divider from "../Divider";
 
 export default function Skills() {
   return (
-    <section className="px-6">
-      <div className="flex flex-col gap-6 px-6 py-16 md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3 xl:gap-12">
+    <section>
+      <div className="skill flex flex-col gap-6 px-6 pb-10 pt-16 md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3 xl:gap-12">
         {SKILL.map((item) => (
           <div
             key={item.label}
-            className="mt-4 flex flex-col items-center justify-center gap-1 md:items-start md:gap-4"
+            className="mt-4 flex flex-col items-center justify-center gap-1 md:items-start md:gap-4 lg:px-10"
           >
             <h3 className="text-pure-white md:text-fs-48 md:-tracking-ls-1.5 md:leading-lh-56 text-fs-32 leading-lh-40 -tracking-ls-1 font-bold">
               {item.label}
@@ -19,7 +19,9 @@ export default function Skills() {
           </div>
         ))}
       </div>
-      <Divider />
+      <div className="px-6 md:hidden">
+        <Divider />
+      </div>
     </section>
   );
 }
