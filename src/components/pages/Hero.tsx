195ab93ba839a5105f4dba1fa5a6a5e2436ada69
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 14:46:43 +0900

    hero done md & lg

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -4,34 +4,38 @@ import NavLink from "../Navlink";
 
 export default function Hero() {
   return (
-    <main className="md:h-screen">
+    <main>
       <div className="hero">
         <div className="flex flex-col items-center pb-16 md:items-stretch">
           <div>
             <NavLink />
-            <div className="relative -mt-40 flex flex-col items-center md:-mt-16 md:items-end">
+            <div className="relative -mt-40 flex flex-col items-center md:items-end">
               <img
-                className="w-52 md:absolute md:w-80"
+                className="z-0 w-52 md:absolute md:w-80"
                 src="/image-profile-mobile.webp"
                 alt="profile picture"
               />
             </div>
           </div>
-          <div className="mt-8 flex flex-col gap-5">
-            <h1 className="text-pure-white text-fs-40 leading-lh-40 -tracking-ls-1.136 text-center font-bold md:text-start">
-              Nice to meet you! I’m{" "}
+          <div className="z-10 mt-8 flex flex-col gap-5 md:mt-28 md:w-[500px] md:gap-0 md:pl-6 lg:w-[700px] lg:pl-16">
+            <h1 className="text-pure-white md:text-fs-72 md:-tracking-ls-2.045 md:leading-lh-72 text-fs-40 leading-lh-40 -tracking-ls-1.136 text-center font-bold md:text-start">
+              Nice to <br className="hidden md:block" /> meet you! I’m{" "}
               <span className="border-green border-b-4">Adam Keyes</span>.
             </h1>
-            <p className="text-pure-white text-fs-16 leading-lh-26 text-center md:text-start">
-              Based in the UK, I’m a front-end developer passionate about
-              building accessible web apps that users love.
+            <p className="text-pure-white text-fs-16 md:text-fs-18 md:leading-lh-28 leading-lh-26 text-center md:mt-16 md:text-start">
+              Based in the UK, I’m a front-end developer{" "}
+              <br className="hidden md:block" /> passionate about building
+              accessible web apps <br className="hidden md:block" /> that users
+              love.
             </p>
           </div>
-          <div className="mt-8">
+          <div className="mt-8 md:mt-11 md:pl-6 lg:pl-16">
             <Button />
           </div>
         </div>
-        <Divider />
+        <div className="px-6 lg:px-16">
+          <Divider />
+        </div>
       </div>
     </main>
   );
