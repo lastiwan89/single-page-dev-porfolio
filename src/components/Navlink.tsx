bfad53bdf9a43202f1828a950daaef2ea98ae0f4
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 14:46:21 +0900

    navlink done padding lg

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -3,7 +3,7 @@ import { ICONS } from "../constant";
 export default function NavLink() {
   return (
     <div>
-      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between md:px-6">
+      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between md:px-6 lg:px-16">
         <div>
           <h3 className="text-pure-white text-fs-24 leading-lh-32 md:text-fs-32 -tracking-ls-0.333 md:-tracking-ls-0.444 font-bold">
             adamkeyes
