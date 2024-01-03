c8abec526d4f545f1d1f1c29dd1a63b19a444e62
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-03 10:04:27 +0900

    update z index and cursor type

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -5,13 +5,17 @@ export default function NavLink({ top }: { top?: string }) {
     <div>
       <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between md:px-6 lg:px-16 xl:px-28">
         <div>
-          <h3 className="text-pure-white text-fs-24 leading-lh-32 md:text-fs-32 -tracking-ls-0.333 md:-tracking-ls-0.444 font-bold">
+          <h3 className="text-white-lighter cursor-pointer text-fs-24 font-bold leading-lh-32 -tracking-ls-0.333 md:text-fs-32 md:-tracking-ls-0.444">
             adamkeyes
           </h3>
         </div>
         <div className={`${top} flex items-center gap-6`}>
           {ICONS.map((icon) => (
-            <img src={icon.url} alt={`${icon.label} icon`} />
+            <img
+              className="cursor-pointer"
+              src={icon.url}
+              alt={`${icon.label} icon`}
+            />
           ))}
         </div>
       </div>
--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -8,7 +8,8 @@ export default function Hero() {
       <div className="hero">
         <div className="flex flex-col items-center pb-16 md:items-stretch">
           <div>
-            <NavLink top="mr-6" />
+            {/* updated z index to make front of image */}
+            <NavLink top="mr-6  z-10" />
             <div className="relative -mt-40 flex flex-col items-center md:items-end">
               <img
                 className="z-0 w-52 md:absolute md:w-80 lg:right-16 xl:right-28"
@@ -18,11 +19,11 @@ export default function Hero() {
             </div>
           </div>
           <div className="z-10 mt-8 flex flex-col gap-5 md:mt-28 md:w-[500px] md:gap-0 md:pl-6 lg:w-[700px] lg:pl-16 xl:pl-28">
-            <h1 className="text-center text-fs-40 font-bold leading-lh-40 -tracking-ls-1.136 text-pure-white md:text-start md:text-fs-72 md:leading-lh-72 md:-tracking-ls-2.045">
+            <h1 className="text-white-lighter text-center text-fs-40 font-bold leading-lh-40 -tracking-ls-1.136 md:text-start md:text-fs-72 md:leading-lh-72 md:-tracking-ls-2.045">
               Nice to <br className="hidden md:block" /> meet you! I’m{" "}
               <span className="border-b-4 border-green">Adam Keyes</span>.
             </h1>
-            <p className="text-center text-fs-16 leading-lh-26 text-pure-white md:mt-16 md:text-start md:text-fs-18 md:leading-lh-28">
+            <p className="text-white-lighter text-center text-fs-16 leading-lh-26 md:mt-16 md:text-start md:text-fs-18 md:leading-lh-28">
               Based in the UK, I’m a front-end developer{" "}
               <br className="hidden md:block" /> passionate about building
               accessible web apps <br className="hidden md:block" /> that users
