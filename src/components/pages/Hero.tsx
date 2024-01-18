de68380222542cf21aebed49b5157f2c6808d5f8
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-19 05:33:49 +0900

    add underline style thickness

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -19,11 +19,14 @@ export default function Hero() {
             </div>
           </div>
           <div className="z-10 mt-8 flex flex-col gap-5 md:mt-28 md:w-[500px] md:gap-0 md:pl-6 lg:w-[700px] lg:pl-16 xl:pl-28">
-            <h1 className="text-white-lighter text-center text-fs-40 font-bold leading-lh-40 -tracking-ls-1.136 md:text-start md:text-fs-72 md:leading-lh-72 md:-tracking-ls-2.045">
+            <h1 className="text-center text-fs-40 font-bold leading-lh-40 -tracking-ls-1.136 text-white-lighter md:text-start md:text-fs-72 md:leading-lh-72 md:-tracking-ls-2.045">
               Nice to <br className="hidden md:block" /> meet you! I’m{" "}
-              <span className="border-b-4 border-green">Adam Keyes</span>.
+              <span className="underline decoration-green decoration-8">
+                Adam Keyes
+              </span>
+              .
             </h1>
-            <p className="text-white-lighter text-center text-fs-16 leading-lh-26 md:mt-16 md:text-start md:text-fs-18 md:leading-lh-28">
+            <p className="text-center text-fs-16 leading-lh-26 text-white-lighter md:mt-16 md:text-start md:text-fs-18 md:leading-lh-28">
               Based in the UK, I’m a front-end developer{" "}
               <br className="hidden md:block" /> passionate about building
               accessible web apps <br className="hidden md:block" /> that users
